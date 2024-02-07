574a81400721cae6cf225a4a6f5ca0d669bcfcfb
Jarvis <94354305+Jarvis-69@users.noreply.github.com>
2024-02-07 16:54:35 +0100

    First push

---
+++ my-app/src/Component/Navbar.js
@@ -0,0 +1,14 @@
+import '../Style/Navbar.css';
+
+function Navbar () {
+    return (
+        <ul>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#news">News</a></li>
+            <li><a href="#contact">Contact</a></li>
+            <li><a href="#about">About</a></li>
+        </ul>
+    );
+}
+
+export default Navbar;
