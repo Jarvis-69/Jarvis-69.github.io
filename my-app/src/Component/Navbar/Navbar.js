854af2ce63fb9528baa8044ee92d95da65a70703
Jarvis <94354305+Jarvis-69@users.noreply.github.com>
2024-03-19 02:14:43 +0100

    ui update

--- my-app/src/Component/Navbar/Navbar.js
+++ my-app/src/Component/Navbar/Navbar.js
@@ -1,15 +1,20 @@
 import './Navbar.css';
 import logo from '../Assests/logo.png';
+import facebook from '../Assests/facebook.png';
+import instagram from '../Assests/instagram.png';
+import logo_black from '../Assests/logo_black.svg';
 
 function Navbar () {
     return (
         <div>
             <ul>
+                <img src={logo_black} alt="Logo" className='navbar_logo' />
                 <li><a href="#home">A propos</a></li>
                 <li><a href="#Services">Services</a></li>
                 <li><a href="#Programmes">Programmes</a></li>
                 <li><a href="#about">Contacts</a></li>
-                <img src={logo} alt="Logo" className='navbar_logo' />
+                <img src={facebook} alt="Facebook" className='navbar_facebook' />
+                <img src={instagram} alt="Instagram" className='navbar_instagram' />
             </ul>
         </div>
     );
--- my-app/src/Component/Présentation/Presentation.js
+++ my-app/src/Component/Présentation/Presentation.js
@@ -1,18 +1,20 @@
 import './Presentation.css';
 import logo from '../Assests/logo.png';
+import horse from '../Assests/horse.png';
 
 function Presentation () {
     return (
         <div className='presentation'>
-            <div>
-                <img src={logo} alt="Logo" className='Logo_Presentation' />
-            </div>
+            <img src={horse} alt="Logo" className='Logo_Presentation' />
             <div className='presentation_txt'>
-                <h1 className='presentation_h1'>SALVADOR INOLA <br></br> Monitrice Indépendante</h1>
-                <p className='presentation_p'>Je suis monitrice indépendante, je vous propose des cours de dressage, de saut d'obstacle et de cross. Je vous propose également des stages de perfectionnement. Je me déplace dans les écuries de votre choix.
-                </p>
-                <button className='presentation_button'>Découvrir</button>
-                <button className='presentation_button' id='presentation_button_2'>Contact</button>
+                <div>
+                    <h1 className='presentation_h1'>Salvador Inola</h1>
+                    <h2 className='presentation_h2'>Monitrice indépendante</h2>
+                    <p className='presentation_p'>Je suis monitrice indépendante, je vous propose des cours de dressage, de saut d'obstacle et de cross. Je vous propose également des stages de perfectionnement. Je me déplace dans les écuries de votre choix.
+                    </p>
+                    <button className='btn'>Découvrir</button>
+                    <button className='btn' id='presentation_button_2'>Contact</button>
+                </div>
             </div>
         </div>
     );
