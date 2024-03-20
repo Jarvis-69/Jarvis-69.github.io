7a2e5c5d21d5de99888e21312b26c0ad40ab2123
Jarvis <94354305+Jarvis-69@users.noreply.github.com>
2024-03-20 03:17:54 +0100

    scrolling button

--- my-app/src/Component/Présentation/Presentation.js
+++ my-app/src/Component/Présentation/Presentation.js
@@ -4,16 +4,22 @@ import horse from '../Assests/horse.png';
 function Presentation () {
     return (
         <div className='presentation'>
-            <img src={horse} alt="Logo" className='Logo_Presentation' />
-            <div className='presentation_txt'>
-                <div>
-                    <h1 className='presentation_h1'>Salvador Inola</h1>
-                    <h2 className='presentation_h2'>Monitrice indépendante</h2>
-                    <p className='presentation_p'>Je suis monitrice indépendante, je vous propose des cours de dressage, de saut d'obstacle et de cross. Je vous propose également des stages de perfectionnement. Je me déplace dans les écuries de votre choix.
-                    </p>
-                    <button className='btn'>Découvrir</button>
-                    <button className='btn' id='presentation_button_2'>Contact</button>
+            <div className='presentation_grid'>
+                <img src={horse} alt="Logo" className='Logo_Presentation' />
+                <div className='presentation_txt'>
+                    <div>
+                        <h1 className='presentation_h1'>Salvador Inola</h1>
+                        <h2 className='presentation_h2'>Monitrice indépendante</h2>
+                        <p className='presentation_p'>Je suis monitrice indépendante, je vous propose des cours de dressage, de saut d'obstacle et de cross. Je vous propose également des stages de perfectionnement. Je me déplace dans les écuries de votre choix.
+                        </p>
+                        <button className='btn'>Découvrir</button>
+                        <button className='btn' id='presentation_button_2'>Contact</button>
+                    </div>
                 </div>
+            </div>
+            <div className='scroll'>
+                <div className="mouse"></div>
+                <p className='scroll_p'>Scroll</p>
             </div>
         </div>
     );
